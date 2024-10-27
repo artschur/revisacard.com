299692fa4798f2a991f16a0485bcd8d93f9d76c4
Arthur Schurhaus <arthurschur@icloud.com>
2024-10-27 15:45:08 -0300

    grafia

--- app/data/items.tsx
+++ app/data/items.tsx
@@ -1,62 +1,55 @@
 import {
-    IconTrophy,
-    IconBrain,
-    IconClock,
-    IconUsers,
-    IconChartBarPopular,
-    IconStar,
-    IconRocket,
-    Icon360,
-    IconExposure,
-    IconAbacus,
-    IconAerialLift,
-  } from "@tabler/icons-react";
+  IconTrophy,
+  IconBrain,
+  IconClock,
+  IconUsers,
+  IconChartBarPopular,
+  IconRocket,
+  IconAerialLift,
+} from "@tabler/icons-react";
 
-import { Skeleton } from "@/components/Skeleton";
-import Image from "next/image";
 
 export const items = [
-    {
-      title: "Estude com Flashcards",
-      description: "Estude com a técnica de repetição espaçada, comprovada cientificamente para melhorar a retenção em até 50% em estudos de longo prazo.",
-      url: 'https://images.unsplash.com/photo-1496469888073-80de7e952517?q=80&w=3432&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      icon: <IconTrophy className="h-4 w-4 text-yellow-500" />,
-    },
-    {
-      title: "Validado por Harvard e Princeton",
-      description: " Pesquisas de Harvard e Princeton demonstram que a repetição espaçada pode melhorar as notas em até 30% em testes de longo prazo.",
-      url: 'https://images.unsplash.com/photo-1609345635744-a58486ce86cd?q=80&w=3311&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      icon: <IconBrain className="h-4 w-4 text-pink-500" />,
-    },
-    {
-      title: "Economia de Tempo",
-      description: "2 horas com flashcards equivalem a mais de 8 horas de revisão tradicional.",
-      url: 'https://images.unsplash.com/photo-1539800644988-3424dd648c2b?q=80&w=3318&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      icon: <IconClock className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "+1 milhão de estudantes",
-      description: "Junte-se a mais de 1 milhão de estudantes ao redor do mundo que usam flashcards com repetição espaçada para melhorar o aprendizado e reter conteúdo de longo prazo.",
-      url: 'https://images.unsplash.com/photo-1524178232363-1fb2b075b655?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      icon: <IconUsers className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "Resultados Reais",
-      description: "A repetição espaçada pode aumentar a retenção em até 50% e melhorar notas em 30%, comprovado por pesquisas de UC San Diego e Princeton.",
-      url: 'https://images.unsplash.com/photo-1606326608606-aa0b62935f2b?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      icon: <IconChartBarPopular className="h-4 w-4 text-green-400" />,
-    },
-    {
-      title: "Metodologia Inovadoras",
-      description: "Utilizamos uma metodologia que combina visualização e repetição para maximizar a retenção e a compreensão do conteúdo.",
-      url: 'https://images.unsplash.com/photo-1633723251072-b9cf36493637?q=80&w=3032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      icon: <IconAerialLift className="h-4 w-4 text-blue-600" />,
-    },
-    {
-      title: "Comece Agora",
-      description: "7 dias de garantia. Resultados ou seu dinheiro de volta.",
-      url: 'https://images.unsplash.com/photo-1614850523503-6e4ef43bee40?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      icon: <IconRocket className="h-4 w-4 text-red-500" />,
-    },
-  ];
-  
+  {
+    title: "Estude com Flashcards",
+    description: "Estude com a técnica de repetição espaçada, comprovada cientificamente para melhorar a retenção em até 50% em estudos de longo prazo.",
+    url: 'https://images.unsplash.com/photo-1496469888073-80de7e952517?q=80&w=3432&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    icon: <IconTrophy className="h-4 w-4 text-yellow-500" />,
+  },
+  {
+    title: "Comprovado por Harvard e Princeton",
+    description: " Pesquisas de Harvard e Princeton demonstram que a repetição espaçada pode melhorar as notas em até 30% em testes de longo prazo.",
+    url: 'https://images.unsplash.com/photo-1609345635744-a58486ce86cd?q=80&w=3311&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    icon: <IconBrain className="h-4 w-4 text-pink-500" />,
+  },
+  {
+    title: "Economia de Tempo",
+    description: "2 horas com flashcards equivalem a mais de 8 horas de revisão tradicional.",
+    url: 'https://images.unsplash.com/photo-1539800644988-3424dd648c2b?q=80&w=3318&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    icon: <IconClock className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "+1 milhão de estudantes",
+    description: "Junte-se a mais de 1 milhão de estudantes ao redor do mundo que usam flashcards com repetição espaçada para melhorar o aprendizado e reter conteúdo de longo prazo.",
+    url: 'https://images.unsplash.com/photo-1524178232363-1fb2b075b655?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    icon: <IconUsers className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "Resultados Reais",
+    description: "A repetição espaçada pode aumentar a retenção em até 50% e melhorar notas em 30%, comprovado por pesquisas de UC San Diego e Princeton.",
+    url: 'https://images.unsplash.com/photo-1606326608606-aa0b62935f2b?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    icon: <IconChartBarPopular className="h-4 w-4 text-green-400" />,
+  },
+  {
+    title: "Metodologia Inovadoras",
+    description: "Utilizamos uma metodologia que combina visualização e repetição para maximizar a retenção e a compreensão do conteúdo.",
+    url: 'https://images.unsplash.com/photo-1633723251072-b9cf36493637?q=80&w=3032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    icon: <IconAerialLift className="h-4 w-4 text-blue-600" />,
+  },
+  {
+    title: "Comece Agora",
+    description: "7 dias de garantia. Resultados ou seu dinheiro de volta.",
+    url: 'https://images.unsplash.com/photo-1614850523503-6e4ef43bee40?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    icon: <IconRocket className="h-4 w-4 text-red-500" />,
+  },
+];
